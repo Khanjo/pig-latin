fdf8569ccac94fe24515b952f31d4deff92e6d5e
Joshua Khan <Khanjo@gmail.com>
2023-06-08 10:43:59 -0700

    add tests > > > co-authored-by: Shawn Cartwright <ponderosaprintingus@gmail.com> > co-authored-by: Jessica Perry <l33thedgieperry@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
 window.addEventListener("load", function (event) {
     let submit = document.getElementById("submit")
-    let text = document.getElementById
+    let text = document.getElementById("text")
+    let output = document.getElementById("output")
+
+    //business logic
+
 })
