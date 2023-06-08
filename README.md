845e4d2eb85125915a922cee0e86130939205c90
Joshua Khan <Khanjo@gmail.com>
2023-06-08 12:03:51 -0700

    pass second test and WIP on third test
    co-authored-by: Shawn Cartwright <ponderosaprintingus@gmail.com>
    co-authored-by: Jessica Perry <l33thedgieperry@gmail.com>

--- README.md
+++ README.md
@@ -3,4 +3,9 @@ Describe: pigLatin()
 Test: "Identify words that begin with a vowel"
 Code: let text = "about"
        pigLatin(text)
-Expected Output: true
+Expected Output: this functions properly 1
+
+Test: "add 'way' to the end of word starting with vowel
+Code: let text = "away"
+      pigLatin(text)
+Expected output: awayway
--- js/scripts.js
+++ js/scripts.js
@@ -9,12 +9,44 @@
      }
 }) */
 let output = document.getElementById("output").innerHTML;
-let text = document.getElementById("text").value;
+let text = [];
 let vowels = ["a", "e", "i", "o", "u"];
 function pigLatin(text) {
-    vowels.forEach(function (element) {
-        if (text.startsWith(element)) {
-            console.log('this functions properly 1');
+    vowels.forEach(function (vowel) {
+        if (text.startsWith(vowel)) {
+            text.forEach(function (singleWord) {
+                singleWord.concat('way');
+            });
+            console.log(text);
         };
     });
 };
+
+
+
+
+
+
+
+/*
+window.onload = function () {
+    vowelAr = ["a", "e", "o", "u", "i"]
+    let form = document.querySelector("form");
+    let div = document.getElementById("output");
+    form.addEventListener("submit", submitFunction);
+
+
+    function submitFunction(event) {
+        event.preventDefault();
+        let inputText = document.getElementById("textinput").value;
+        let savedText = inputText;
+
+        for (index = 0; index < 6; index++) {
+            savedText = savedText.replaceAll(vowelAr[index],'-');
+            div.innerHTML = savedText;
+        }
+    }
+
+
+}
+*/
