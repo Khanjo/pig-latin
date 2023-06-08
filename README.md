99349bdd29086d0c1a4c4b15aaff3a267b32a152
Joshua Khan <Khanjo@gmail.com>
2023-06-08 11:36:48 -0700

    passes first test > > > co-authored-by: Shawn Cartwright <ponderosaprintingus@gmail.com> > co-authored-by: Jessica Perry <l33thedgieperry@gmail.com>

---
+++ README.md
@@ -0,0 +1,6 @@
+Describe: pigLatin()
+
+Test: "Identify words that begin with a vowel"
+Code: let text = "about"
+       pigLatin(text)
+Expected Output: true
--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,20 @@
-window.addEventListener("load", function (event) {
-    let submit = document.getElementById("submit")
-    let text = document.getElementById("text")
-    let output = document.getElementById("output")
+/* window.addEventListener("load", function (event) {
+     let submit = document.getElementById("submit")
+     let text = document.getElementById("text").value;
+     let output = document.getElementById("output")
 
-    //business logic
-
-})
+     //business logic
+     function pigLatin(text) {
+        
+     }
+}) */
+let output = document.getElementById("output").innerHTML;
+let text = document.getElementById("text").value;
+let vowels = ["a", "e", "i", "o", "u"];
+function pigLatin(text) {
+    vowels.forEach(function (element) {
+        if (text.startsWith(element)) {
+            console.log('this functions properly 1');
+        };
+    });
+};
